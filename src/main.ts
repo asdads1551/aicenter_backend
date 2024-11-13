339b0b70f7975b85a0dbf66e64d46ed2949d9004
Nick Chen <b10303008@gmail.com>
2024-11-14 00:10:27 +0800

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
